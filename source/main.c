89f656c240ec520794823ec5343b70d790cba744
David Perez <dpere048@wch136-05.cs.ucr.edu>
2020-04-20 21:32:55 -0700

    done code1

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: dpere048
  *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
+ *	Lab Section:021
+ *	Assignment: Lab #4  Exercise #1
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -11,13 +11,69 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-
+enum States{Start, LED0, LED1, WAIT} state;
+unsigned char button;
+unsigned char tmpA;
+void Led_Tick(){
+	switch(state){
+	case START:
+		if(button == 1){
+		state = LED1;
+		}
+		else{
+		state = START;
+		}
+		break;
+	case LED0:
+		if(button == 1){
+		state = LED0;
+		}
+		else{
+		state = START;
+		}
+		break;
+	case LED1:
+		if(button == 1){
+		state = LED1;
+		}
+		else{
+		state = WAIT;
+		}
+		break;
+	case WAIT:
+		if(button == 1){
+		state = LED0;
+		}
+		else{
+		state = WAIT;
+		}
+		break;
+	}
+	switch(state){
+	case START:
+		tmpA = 0x01; //PB0 is on
+	break;
+	case LED0:
+		tmpA = 0x01; //PB0 is on
+	break;
+	case LED1:
+		tmpA = 0x02; //PB1 is on
+	break;
+	case WAIT:
+		tmpA = 0x02; //PB1 is on
+	break;
+	}
+}
 int main(void) {
     /* Insert DDR and PORT initializations */
-
+DDRA = 0x00; PORTA = 0xFF;
+DDRB = 0xFF; PORTB = 0x00;
+state = Start;
     /* Insert your solution below */
     while (1) {
-
+	button = PINA&0x01;//PA0
+	led_Tick();
+	PORTB = tmpA;
     }
     return 1;
 }
